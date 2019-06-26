21921ff36bba387617a1c20c55b1d7e547af8720
toshihiro konda <kontoshi0317@gmail.com>
2019-06-26 19:19:06 +0900

    refactoring and modify select

--- llvm8.0.0/llvm/lib/Target/VideoCore4/VideoCore4PseudoFixup.cpp
+++ llvm8.0.0/llvm/lib/Target/VideoCore4/VideoCore4PseudoFixup.cpp
@@ -25,6 +25,55 @@
 #include <cstdlib>
 #include <iostream>
 
+#define JUMP_COMP_CC(opcode) \
+        unsigned           Reg1 = MI->getOperand(0).getReg(); \
+        unsigned           Reg2 = MI->getOperand(1).getReg(); \
+        MachineBasicBlock *BB   = MI->getOperand(2).getMBB(); \
+							      \
+	MBB.erase(MI);					      \
+							      \
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))      \
+	  .addReg(Reg1)					      \
+	  .addReg(Reg2);				      \
+	BuildMI(MBB, I, dl, TII->get(opcode))  \
+	  .addMBB(BB);
+
+#define	SELECT_CC(opcode_t, opcode_f) \
+        unsigned Reg1 = MI->getOperand(0).getReg(); \
+        unsigned Reg2 = MI->getOperand(1).getReg(); \
+        unsigned Reg3 = MI->getOperand(2).getReg(); \
+        unsigned Reg4 = MI->getOperand(3).getReg(); \
+        unsigned Reg5 = MI->getOperand(4).getReg(); \
+						    \
+        MBB.erase(MI);				    \
+						    \
+	if (Reg1 == Reg3) {			    \
+	  BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))	   \
+	    .addReg(Reg4)					   \
+	    .addReg(Reg5);					   \
+	  BuildMI(MBB, I, dl, TII->get(opcode_t))		   \
+	    .addReg(Reg1)					   \
+	    .addReg(Reg2);					   \
+	} else if (Reg1 == Reg2) {				   \
+	  BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))	   \
+	    .addReg(Reg4)					   \
+	    .addReg(Reg5);					   \
+	  BuildMI(MBB, I, dl, TII->get(opcode_f))		   \
+	    .addReg(Reg1)					   \
+	    .addReg(Reg3);					   \
+	} else {						   \
+	  BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))	   \
+	    .addReg(Reg4)					   \
+	    .addReg(Reg5);					   \
+	  BuildMI(MBB, I, dl, TII->get(opcode_t))		   \
+	    .addReg(Reg1)					   \
+	    .addReg(Reg2);					   \
+ 	  BuildMI(MBB, I, dl, TII->get(opcode_f))		   \
+	    .addReg(Reg1)					   \
+	    .addReg(Reg3);					   \
+	}
+
+
 using namespace llvm;
 
 namespace llvm {
@@ -76,171 +125,122 @@ VideoCore4PseudoFixup::runOnMachineBasicBlock(MachineBasicBlock &MBB) {
     switch (Opcode) {
     case VideoCore4::JMP_COMP_EQ_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_EQ))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_EQ);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_NE_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_NE))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_NE);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_GT_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_GT))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_GT);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_GE_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_GE))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_GE);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_LT_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_LT))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_LT);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_LE_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_LE))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_LE);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_HI_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_HI))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_HI);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_HS_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_HS))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_HS);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_LO_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_LO))
-	  .addMBB(BB);
-
+	JUMP_COMP_CC(VideoCore4::JMP_CC_LO);
 	Changed = true;
 	break;
       }
     case VideoCore4::JMP_COMP_LS_P:
       {
-        unsigned           Reg1 = MI->getOperand(0).getReg();
-        unsigned           Reg2 = MI->getOperand(1).getReg();
-        MachineBasicBlock *BB   = MI->getOperand(2).getMBB();
-	
-        MBB.erase(MI);
-	
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::CMP_P))
-	  .addReg(Reg1)
-	  .addReg(Reg2);
-	BuildMI(MBB, I, dl, TII->get(VideoCore4::JMP_CC_LS))
-	  .addMBB(BB);
+	JUMP_COMP_CC(VideoCore4::JMP_CC_LS);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_EQ_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_EQ_P, VideoCore4::CMOV_NE_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_NE_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_NE_P, VideoCore4::CMOV_EQ_P);
+	Changed = true;
+	break;
+      }
 
+    case VideoCore4::SELECT_GT_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_GT_P, VideoCore4::CMOV_LE_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_GE_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_GE_P, VideoCore4::CMOV_LT_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_LT_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_LT_P, VideoCore4::CMOV_GE_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_LE_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_LE_P, VideoCore4::CMOV_GT_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_HI_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_HI_P, VideoCore4::CMOV_LS_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_HS_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_HS_P, VideoCore4::CMOV_LO_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_LO_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_LO_P, VideoCore4::CMOV_HS_P);
+	Changed = true;
+	break;
+      }
+    case VideoCore4::SELECT_LS_P:
+      {
+	SELECT_CC(VideoCore4::CMOV_LS_P, VideoCore4::CMOV_HI_P);
 	Changed = true;
 	break;
       }
