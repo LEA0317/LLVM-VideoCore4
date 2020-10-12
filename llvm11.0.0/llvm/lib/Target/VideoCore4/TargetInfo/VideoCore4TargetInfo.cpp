c97edaa2154cb99a3b4630053fbed67cb723ab96
toshihiro konda <kontoshi0317@gmail.com>
2020-10-13 03:47:23 +0900

    port to llvm11

---
+++ llvm11.0.0/llvm/lib/Target/VideoCore4/TargetInfo/VideoCore4TargetInfo.cpp
@@ -0,0 +1,22 @@
+//===-- VideoCore4TargetInfo.cpp - VideoCore4 Target Implementation -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "VideoCore4.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/TargetRegistry.h"
+using namespace llvm;
+
+Target llvm::TheVideoCore4Target;
+
+extern "C" void LLVMInitializeVideoCore4TargetInfo() {
+  RegisterTarget<Triple::videocore, /*HasJIT=*/true> X(TheVideoCore4Target,
+                                                       "videocore",
+                                                       "VideoCore LittleEndian",
+                                                       "VideoCore");
+}
